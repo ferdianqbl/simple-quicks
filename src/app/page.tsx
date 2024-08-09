6775737291132bfdf4fc9584d5511452c05c6361
ferdianqbl <muhammadferdianiqbal@gmail.com>
2024-08-09 21:23:00 +0700

    feat: create layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
-export default function Home() {
-  return <main className="container h-screen">test</main>;
-}
+import { Button } from "@/components/ui/button";
+import { Zap } from "lucide-react";
+
+export const Page = () => {
+  return (
+    <main className="container h-screen flex py-8">
+      <Button
+        size={"icon"}
+        className="self-end ms-auto bg-primary-100 hover:bg-primary-100/80"
+      >
+        <Zap className="w-6 h-6" />
+      </Button>
+    </main>
+  );
+};
+
+export default Page;
